78b3b774d8ecafe34fd4d744d3ecd2cfc4fd15c9
terridb <terrideboer99@gmail.com>
2025-02-07 15:41:28 +0100

    Basis en CSS gedaan

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
 import './App.css';
 
 function App() {
     return (
         <div>
-            Maak hier jouw formulier!
+            <form>
+                <fieldset>
+                    <legend>Gegevens</legend>
+                    <label htmlFor="name-field">Naam:
+                        <input type="text" name="name" id="name-field"/>
+                    </label>
+                    <label htmlFor="age-field">Leeftijd:
+                        <input type="number" name="age" id="age-field" value="0"/>
+                    </label>
+                </fieldset>
+                <fieldset>
+                    <legend>Jouw review:</legend>
+                    <label htmlFor="remark-field">Opmerkingen:
+                        <textarea name="remarks" id="remark-field" rows="4" cols="40"
+                                  placeholder="Wat vond je van het recept?"/>
+                    </label>
+                    <label htmlFor="newsletter-field">
+                        <input type="checkbox" name="newsletter" id="newsletter-field"/>
+                        Ik schrijf me in voor de nieuwsbrief
+                    </label>
+                    <button type="submit">Versturen</button>
+                </fieldset>
+
+            </form>
         </div>
     );
 }
