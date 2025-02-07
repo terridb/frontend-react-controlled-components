97f62bf86fd9506255590e456830acabb23a4221
terridb <terrideboer99@gmail.com>
2025-02-07 16:55:47 +0100

    Bonus opdracht afgerond

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,82 @@
 import './App.css';
 import {useState} from "react";
 
+
 function App() {
-    const [nameValue, setNameValue] = useState('');
-    const [ageValue, setAgeValue] = useState(0);
-    const [remarkValue, setRemarkValue] = useState('');
-    const [newsletterValue, setNewsletterValue] = useState(false);
+    const [state, setState] = useState({
+        name: "",
+        age: 0,
+        remarks: "",
+        newsletter: false
+    })
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(nameValue, ageValue, remarkValue, newsletterValue);
+        console.log(state);
+    }
+
+    function handleChange(e) {
+        const value =
+            e.target.type === "checkbox" ? e.target.checked : e.target.value;
+        setState({
+            ...state,
+            [e.target.name]: value
+        });
     }
 
     return (
-        <div>
             <form onSubmit={handleSubmit}>
                 <fieldset>
                     <legend>Gegevens</legend>
                     <label htmlFor="name-field">Naam:
-                        <input type="text" name="name" value={nameValue} id="name-field"
-                        onChange={(e) => setNameValue(e.target.value)}/>
+                        <input
+                            type="text"
+                            name="name"
+                            value={state.name}
+                            id="name-field"
+                            onChange={handleChange}
+                        />
                     </label>
-                    <label htmlFor="age-field">Leeftijd:
-                        <input type="number" name="age" id="age-field" value={ageValue}
-                        onChange={(e) => setAgeValue(parseInt(e.target.value))}/>
+                    <label htmlFor="age-field">
+                        Leeftijd:
+                        <input
+                            type="number"
+                            name="age"
+                            id="age-field"
+                            value={state.age}
+                            onChange={handleChange}
+                        />
                     </label>
                 </fieldset>
                 <fieldset>
                     <legend>Jouw review:</legend>
-                    <label htmlFor="remark-field">Opmerkingen:
-                        <textarea name="remarks" id="remark-field" rows="4" cols="40"
-                                  placeholder="Wat vond je van het recept?" value={remarkValue}
-                                  onChange={(e) => setRemarkValue(e.target.value)}
+                    <label htmlFor="remark-field">
+                        Opmerkingen:
+                        <textarea
+                            name="remarks"
+                            id="remark-field"
+                            rows="4"
+                            cols="40"
+                            placeholder="Wat vond je van het recept?"
+                            value={state.remarks}
+                            onChange={handleChange}
                         />
                     </label>
                     <label htmlFor="newsletter-field">
-                        <input type="checkbox" name="newsletter" id="newsletter-field" checked={newsletterValue}
-                        onChange={(e) => setNewsletterValue(e.target.checked)}
+                        <input
+                            type="checkbox"
+                            name="newsletter"
+                            id="newsletter-field"
+                            checked={state.newsletter}
+                            onChange={handleChange}
                         />
                         Ik schrijf me in voor de nieuwsbrief
                     </label>
-                    <button type="submit">Versturen</button>
+                    <button type="submit">
+                        Versturen
+                    </button>
                 </fieldset>
-
             </form>
-        </div>
     );
 }
 
